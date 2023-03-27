f9e0f5725acbe7480553750c3f57c72e6108c3f4
Robert Taylor <robert.taylor@pegasusimagery.ca>
2023-03-27 11:21:48 -0600

    AP_Periph: Add minerva CAN class

---
+++ Tools/AP_Periph/minervaCAN.cpp
@@ -0,0 +1,368 @@
+/*
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Robert Taylor / Pegasus Imagery Ltd
+ */
+
+
+#include <AP_HAL/AP_HAL.h>
+
+#include "minervaCAN.h"
+#if PERIPH_MINERVA_CAN_ENABLE
+
+#include <AP_Param/AP_Param.h>
+#include <AP_CANManager/AP_CANManager.h>
+#include <AP_Common/AP_Common.h>
+#include <AP_Scheduler/AP_Scheduler.h>
+#include <AP_HAL/utility/sparse-endian.h>
+#include <GCS_MAVLink/GCS.h>
+#include <AP_Logger/AP_Logger.h>
+#include <SRV_Channel/SRV_Channel.h>
+
+#include <AP_EFI/AP_EFI_Currawong_ECU.h>
+
+#include <AP_PiccoloCAN/piccolo_protocol/ECUPackets.h>
+
+#include <stdio.h>
+
+extern const AP_HAL::HAL& hal;
+
+#if HAL_CANMANAGER_ENABLED
+#define debug_can(level_debug, fmt, args...) do { AP::can().log_text(level_debug, "MinervaCAN", fmt, ##args); } while (0)
+#else
+#define debug_can(level_debug, fmt, args...)
+#endif
+
+// table of user-configurable Minerva CAN bus parameters
+const AP_Param::GroupInfo AP_MinervaCAN::var_info[] = {
+#if AP_EFI_CURRAWONG_ECU_ENABLED
+    // @Param: ECU_ID
+    // @DisplayName: ECU Node ID
+    // @Description: Node ID to send ECU throttle messages to. Set to zero to disable ECU throttle messages. Set to 255 to broadcast to all ECUs.
+    // @Range: 0 255
+    // @User: Advanced
+    AP_GROUPINFO("ECU_ID", 5, AP_MinervaCAN, _ecu_id, PICCOLO_CAN_ECU_ID_DEFAULT), // Since this is a piccoloCAN ECU we'll just use their definition
+
+    // @Param: ECU_RT
+    // @DisplayName: ECU command output rate
+    // @Description: Output rate of ECU command messages
+    // @Units: Hz
+    // @User: Advanced
+    // @Range: 1 500
+    AP_GROUPINFO("ECU_RT", 6, AP_MinervaCAN, _ecu_hz, MINERVA_MSG_RATE_HZ_DEFAULT),
+#endif
+    AP_GROUPEND
+};
+
+AP_MinervaCAN::AP_MinervaCAN()
+{
+    AP_Param::setup_object_defaults(this, var_info);
+
+    debug_can(AP_CANManager::LOG_INFO, "MinervaCAN: constructed\n\r");
+}
+
+AP_MinervaCAN::~AP_MinervaCAN() { }
+
+AP_MinervaCAN *AP_MinervaCAN::get_pcan(uint8_t driver_index)
+{
+    if (driver_index >= AP::can().get_num_drivers() ||
+        AP::can().get_driver_type(driver_index) != MINERVA_CAN_PROTOCOL) {
+        return nullptr;
+    }
+
+    return static_cast<AP_MinervaCAN*>(AP::can().get_driver(driver_index));
+}
+
+bool AP_MinervaCAN::add_interface(AP_HAL::CANIface* can_iface) {
+    if (_can_iface != nullptr) {
+        debug_can(AP_CANManager::LOG_ERROR, "MinervaCAN: Multiple Interface not supported\n\r");
+        return false;
+    }
+
+    _can_iface = can_iface;
+
+    if (_can_iface == nullptr) {
+        debug_can(AP_CANManager::LOG_ERROR, "MinervaCAN: CAN driver not found\n\r");
+        return false;
+    }
+
+    if (!_can_iface->is_initialized()) {
+        debug_can(AP_CANManager::LOG_ERROR, "MinervaCAN: Driver not initialized\n\r");
+        return false;
+    }
+
+    if (!_can_iface->set_event_handle(&_event_handle)) {
+        debug_can(AP_CANManager::LOG_ERROR, "MinervaCAN: Cannot add event handle\n\r");
+        return false;
+    }
+    return true;
+}
+
+// initialize MinervaCAN bus
+void AP_MinervaCAN::init(uint8_t driver_index, bool enable_filters)
+{
+    _driver_index = driver_index;
+
+    debug_can(AP_CANManager::LOG_DEBUG, "MinervaCAN: starting init\n\r");
+
+    if (_initialized) {
+        debug_can(AP_CANManager::LOG_ERROR, "MinervaCAN: already initialized\n\r");
+        return;
+    }
+    snprintf(_thread_name, sizeof(_thread_name), "MinervaCAN_%u", driver_index);
+
+    // start calls to loop in separate thread
+    if (!hal.scheduler->thread_create(FUNCTOR_BIND_MEMBER(&AP_MinervaCAN::loop, void), _thread_name, 4096, AP_HAL::Scheduler::PRIORITY_MAIN, 1)) {
+        debug_can(AP_CANManager::LOG_ERROR, "MinervaCAN: couldn't create thread\n\r");
+        return;
+    }
+
+    _initialized = true;
+
+    debug_can(AP_CANManager::LOG_DEBUG, "MinervaCAN: init done\n\r");
+}
+
+// loop to send output to CAN devices in background thread
+void AP_MinervaCAN::loop()
+{
+    AP_HAL::CANFrame txFrame {};
+    AP_HAL::CANFrame rxFrame {};
+
+#if HAL_EFI_CURRAWONG_ECU_ENABLED
+    uint16_t ecu_tx_counter = 0;
+#endif
+
+    // CAN Frame ID components
+    uint8_t frame_id_group;     // Minerva message group
+    uint16_t frame_id_device;   // Device identifier
+
+    while (true) {
+
+        if (!_initialized) {
+            debug_can(AP_CANManager::LOG_ERROR, "MinervaCAN: not initialized\n\r");
+            hal.scheduler->delay_microseconds(10000);
+            continue;
+        }
+
+#if HAL_EFI_CURRAWONG_ECU_ENABLED
+        _ecu_hz.set(constrain_int16(_ecu_hz, MINERVA_MSG_RATE_HZ_MIN, MINERVA_MSG_RATE_HZ_MAX));
+
+        uint16_t ecuCmdRateMs = 1000 / _ecu_hz;
+#endif
+        uint64_t timeout = AP_HAL::micros64() + 250ULL;
+
+        // 1ms loop delay
+        hal.scheduler->delay_microseconds(1000);
+
+#if HAL_EFI_CURRAWONG_ECU_ENABLED
+        // Transmit ecu throttle commands at regular intervals
+        if (ecu_tx_counter++ > ecuCmdRateMs) {
+            ecu_tx_counter = 0;
+            send_ecu_messages();
+        }
+#endif
+
+        // Look for any message responses on the CAN bus
+        while (read_frame(rxFrame, timeout)) {
+            // Extract group and device ID values from the frame identifier
+            frame_id_group = (rxFrame.id >> 24) & 0x1F;
+            frame_id_device = (rxFrame.id >> 8) & 0xFF;
+            (void)frame_id_device; // Get rid of unused warning
+            // Only accept extended messages
+            if ((rxFrame.id & AP_HAL::CANFrame::FlagEFF) == 0) {
+                continue;
+            }
+
+            switch (MessageGroup(frame_id_group)) {
+            case MessageGroup::ECU_OUT:
+            #if HAL_EFI_CURRAWONG_ECU_ENABLED
+                if (handle_ecu_message(rxFrame)) {
+                    // Returns true if the message was successfully decoded
+                }
+            #endif
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
+
+// write frame on CAN bus, returns true on success
+bool AP_MinervaCAN::write_frame(AP_HAL::CANFrame &out_frame, uint64_t timeout)
+{
+    if (!_initialized) {
+        debug_can(AP_CANManager::LOG_ERROR, "MinervaCAN: Driver not initialized for write_frame\n\r");
+        return false;
+    }
+
+    bool read_select = false;
+    bool write_select = true;
+    
+    bool ret =  _can_iface->select(read_select, write_select, &out_frame, timeout);
+
+    if (!ret || !write_select) {
+        return false;
+    }
+
+    return (_can_iface->send(out_frame, timeout, AP_HAL::CANIface::AbortOnError) == 1);
+}
+
+// read frame on CAN bus, returns true on succses
+bool AP_MinervaCAN::read_frame(AP_HAL::CANFrame &recv_frame, uint64_t timeout)
+{
+    if (!_initialized) {
+        debug_can(AP_CANManager::LOG_ERROR, "MinervaCAN: Driver not initialized for read_frame\n\r");
+        return false;
+    }
+    bool read_select = true;
+    bool write_select = false;
+    bool ret = _can_iface->select(read_select, write_select, nullptr, timeout);
+
+    if (!ret || !read_select) {
+        // No frame available
+        return false;
+    }
+
+    uint64_t time;
+    AP_HAL::CANIface::CanIOFlags flags {};
+
+    return (_can_iface->receive(recv_frame, time, flags) == 1);
+}
+
+// called from SRV_Channels
+void AP_MinervaCAN::update()
+{
+    uint64_t timestamp = AP_HAL::micros64();
+
+#if HAL_EFI_CURRAWONG_ECU_ENABLED
+    if (_ecu_id != 0) {
+        _ecu_info.command = SRV_Channels::get_output_scaled(SRV_Channel::k_throttle); 
+        _ecu_info.newCommand = true;
+    }
+#endif // HAL_EFI_CURRAWONG_ECU_ENABLED
+
+    AP_Logger *logger = AP_Logger::get_singleton();
+
+    // Push received telemetry data into the logging system
+    if (logger && logger->logging_enabled()) {
+
+        WITH_SEMAPHORE(_telem_sem);
+        (void)timestamp; // We still might want this, remove the warnings
+    }
+}
+
+#if HAL_EFI_CURRAWONG_ECU_ENABLED
+void AP_MinervaCAN::send_ecu_messages(void)
+{
+    AP_HAL::CANFrame txFrame {};
+
+    const uint64_t timeout = AP_HAL::micros64() + 1000ULL;
+
+    // No ECU node id set, don't send anything
+    if (_ecu_id == 0) {
+        printf("\r\nNo ECU ID\r\n");
+        return;
+    }
+
+    if (_ecu_info.newCommand) {
+        printf("\r\nSending ECU command\r\n");
+        encodeECU_ThrottleCommandPacket(&txFrame, _ecu_info.command);
+        txFrame.id |= (uint8_t) _ecu_id;
+
+        _ecu_info.newCommand = false;
+
+        write_frame(txFrame, timeout);
+    }
+}
+
+bool AP_MinervaCAN::handle_ecu_message(AP_HAL::CANFrame &frame)
+{
+    // Get the ecu instance
+    AP_EFI_Currawong_ECU* ecu = AP_EFI_Currawong_ECU::get_instance();
+    if (ecu != nullptr) {
+        return ecu->handle_message(frame);
+    }
+    return false;
+}
+#endif // HAL_EFI_CURRAWONG_ECU_ENABLED
+
+/* Minerva Glue Logic
+ * The following functions are required by the auto-generated protogen code.
+ */
+
+
+//! \return the packet data pointer from the packet
+uint8_t* getECUPacketData(void* pkt)
+{
+    AP_HAL::CANFrame* frame = (AP_HAL::CANFrame*) pkt;
+
+    return (uint8_t*) frame->data;
+}
+
+//! \return the packet data pointer from the packet, const
+const uint8_t* getECUPacketDataConst(const void* pkt)
+{
+    AP_HAL::CANFrame* frame = (AP_HAL::CANFrame*) pkt;
+
+    return (const uint8_t*) frame->data;
+}
+
+//! Complete a packet after the data have been encoded
+void finishECUPacket(void* pkt, int size, uint32_t packetID)
+{
+    AP_HAL::CANFrame* frame = (AP_HAL::CANFrame*) pkt;
+
+    if (size > AP_HAL::CANFrame::MaxDataLen) {
+        size = AP_HAL::CANFrame::MaxDataLen;
+    }
+
+    frame->dlc = size;
+
+    /* Encode the CAN ID
+     * 0x09mmdddd
+     * - 07 = ECU_IN (to and ECU) group ID
+     * - mm = Message ID
+     * - dd = Device ID
+     *
+     * Note: The Device ID (lower 16 bits of the frame ID) will have to be inserted later
+     */
+
+    uint32_t id = (((uint8_t) AP_MinervaCAN::MessageGroup::ECU_IN) << 24) |       // CAN Group ID
+                  ((packetID & 0xFF) << 16);                                       // Message ID
+
+    // Extended frame format
+    id |= AP_HAL::CANFrame::FlagEFF;
+
+    frame->id = id;
+}
+
+//! \return the size of a packet from the packet header
+int getECUPacketSize(const void* pkt)
+{
+    AP_HAL::CANFrame* frame = (AP_HAL::CANFrame*) pkt;
+
+    return (int) frame->dlc;
+}
+
+//! \return the ID of a packet from the packet header
+uint32_t getECUPacketID(const void* pkt)
+{
+    AP_HAL::CANFrame* frame = (AP_HAL::CANFrame*) pkt;
+
+    // Extract the message ID field from the 29-bit ID
+    return (uint32_t) ((frame->id >> 16) & 0xFF);
+}
+
+#endif // HAL_Minerva_CAN_ENABLE
---
+++ Tools/AP_Periph/minervaCAN.h
@@ -0,0 +1,102 @@
+/*
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Robert Taylor / Pegasus Imagery Ltd
+ */
+
+#pragma once
+
+#include <AP_HAL/AP_HAL.h>
+#include <AP_CANManager/AP_CANDriver.h>
+
+#include <AP_Param/AP_Param.h>
+
+#include <AP_EFI/AP_EFI_Currawong_ECU.h>
+
+#if PERIPH_MINERVA_CAN_ENABLE
+
+#define MINERVA_CAN_PROTOCOL AP_CANManager::Driver_Type_Scripting2 // We'll use scripting2 for now since this is a periph
+
+#define MINERVA_MSG_RATE_HZ_MIN 1
+#define MINERVA_MSG_RATE_HZ_MAX 500
+#define MINERVA_MSG_RATE_HZ_DEFAULT 500
+
+#define PICCOLO_CAN_ECU_ID_DEFAULT 255 // This one is kept as piccolo since EFI speaks piccoloCAN
+
+
+class AP_MinervaCAN : public AP_CANDriver
+{
+public:
+    AP_MinervaCAN();
+    ~AP_MinervaCAN();
+
+    // Minerva message groups define the CAN id's needing translation
+    enum class MessageGroup : uint8_t {
+        SIMULATOR = 0x00,       // Simulator messages
+        ECU_OUT = 0x08,         // Messages *from* an ECU Inherited from PiccoloCAN
+        ECU_IN = 0x09,          // Message *to* an ECU Inherited from PiccoloCAN
+
+    };
+
+    struct CurrawongECU_Info_t {
+        float command;
+        bool newCommand;
+    } _ecu_info;
+
+    /* Do not allow copies */
+    CLASS_NO_COPY(AP_MinervaCAN);
+
+    static const struct AP_Param::GroupInfo var_info[];
+
+    // Return PiccoloCAN from @driver_index or nullptr if it's not ready or doesn't exist
+    static AP_MinervaCAN *get_pcan(uint8_t driver_index);
+
+    // initialize PiccoloCAN bus
+    void init(uint8_t driver_index, bool enable_filters) override;
+    bool add_interface(AP_HAL::CANIface* can_iface) override;
+
+    // called from SRV_Channels
+    void update();
+
+private:
+
+    // loop to send output to ESCs in background thread
+    void loop();
+
+    // write frame on CAN bus, returns true on success
+    bool write_frame(AP_HAL::CANFrame &out_frame, uint64_t timeout);
+
+    // read frame on CAN bus, returns true on succses
+    bool read_frame(AP_HAL::CANFrame &recv_frame, uint64_t timeout);
+    
+#if HAL_EFI_CURRAWONG_ECU_ENABLED
+    void send_ecu_messages(void);
+
+    // interpret an ECU message received over CAN
+    bool handle_ecu_message(AP_HAL::CANFrame &frame);
+#endif
+
+    bool _initialized;
+    char _thread_name[16];
+    uint8_t _driver_index;
+    AP_HAL::CANIface* _can_iface;
+    HAL_EventHandle _event_handle;
+
+    AP_Int16 _ecu_id;        //! ECU Node ID
+    AP_Int16 _ecu_hz;       //! ECU update rate (Hz)
+
+    HAL_Semaphore _telem_sem;
+};
+
+#endif // HAL_PICCOLO_CAN_ENABLE
